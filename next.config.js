8c2179bb9606a320b785d7c1dfb54e2701558916
hogana2023 <129881123+hogana2023@users.noreply.github.com>
2025-04-19 21:58:07 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   output: 'standalone',
-  experimental: {
-    // Disable server components for simpler build
-    appDir: true,
-    serverComponentsExternalPackages: [],
-  },
+  
 };
 
 module.exports = nextConfig;
